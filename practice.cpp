cb7089a860c7e4d4e360bccbe9a1baeaff713f1c
vijit-vishnoi <vishnoivijit@gamil.com>
2026-02-17 16:38:19 +0530

    contribution check

---
+++ practice.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    reverse(s.begin(),s.end());
+    int count=0;
+    for(int i=0;i<s.size();i++){
+        count+=(s[i]-'0')*pow(2,i);
+    }
+    cout<<count;
+    
+}
